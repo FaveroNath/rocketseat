fef3287a4c5aedbe6b8a119ef46230d073e27756
FaveroNath <faveronathalia@gmail.com>
2020-09-09 16:18:00 -0300

    ES6

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,8 +1,11 @@
 "use strict";
 
 //Mod 1
-//Exercio 5
-var usuario = 'Diego';
-var idade = 23; //console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
-
-console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos."));
+//Exercio 7
+var nome = 'Diego';
+var idade = 23;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
