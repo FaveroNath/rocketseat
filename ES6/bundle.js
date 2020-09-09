853914d5c38465fd8cd544dca24b8f4b1fcbce02
FaveroNath <faveronathalia@gmail.com>
2020-09-09 15:58:37 -0300

    ES6

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,59 +1,31 @@
 "use strict";
 
 //Mod 1
-//Exercio 3
-var arr = [1, 2, 3, 4, 5];
-/*arr.map(function(item) {
- return item + 10;
-});*/
+//Exercio 4
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
 
-var newArr = arr.map(function (item) {
-  return item + 10;
-});
-console.log(newArr);
-/*function mostraIdade(usuario) {
- return usuario.idade;
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
 }
-mostraIdade(usuario);*/
 
-var usuario = {
+console.log(mostraInfo({
   nome: 'Diego',
   idade: 23
-};
-
-var mostraIdade = function mostraIdade(item) {
-  return usuario.idade;
-};
-
-console.log(mostraIdade());
-/*function mostraUsuario(nome = 'Diego', idade = 18) {
- return { nome, idade };
-}*/
-
-var nome = "Diego";
-var idade = 23;
-
-var mostraUsuario = function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-console.log(mostraUsuario(nome, idade));
-console.log(mostraUsuario(nome));
-/*
-const promise = function() {
- return new Promise(function(resolve, reject) {
- return resolve();
- })
-}
-*/
-
-var promise = function promise() {
-  return new promise(function (resolve, reject) {
-    return resolve();
-  });
-};
+}));
